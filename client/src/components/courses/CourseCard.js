c2ac9c7795288644c785c02ab0082be6dba08ae1
unknown <xypnotica@gmail.com>
2019-06-13 10:20:47 +0200

    card

--- client/src/components/courses/CourseCard.js
+++ client/src/components/courses/CourseCard.js
@@ -3,11 +3,21 @@ import React from "react";
 const CourseCard = () => {
   return (
     <div className="course__card">
-      <img className="course__card__img" src="" alt="slika" />
-      <h3 className="course__card__h3">Kursevi aktivne konverzacije</h3>
-      <p className="course__card__p">
-        Namenjeni onima koji zele da prevazidju blokadu u govoru
-      </p>
+      <div className="course__card__front">
+        <div className="course__card__img">
+          <img className="course__card--img" src="" alt="slika" />
+        </div>
+
+        <h4 className="course__card__h4">Kursevi aktivne konverzije</h4>
+        <p className="course__card__p">
+          Ovo je mala React Js aplikacija. U pitanju je vec poznata igra Pig
+          Game. Dva igraca bacaju kockice i pokusavaju prvi da dostignu odredjen
+          broj pojena koji su prethodno rucno odredili. Igra je pisana u
+          React-u. Kao Context menager je koriscen Contect api. Vise u ovoj
+          aplikaciji mozete videti na njenoj stranici.
+        </p>
+      </div>
+      <div className="course__card__back">BACK</div>
     </div>
   );
 };
