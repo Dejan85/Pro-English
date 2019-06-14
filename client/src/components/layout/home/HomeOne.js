73e6c6200e2087cbdf8b89b81758791d6998c2dd
unknown <xypnotica@gmail.com>
2019-06-14 18:07:32 +0200

    start building landing page-one

--- client/src/components/layout/home/HomeOne.js
+++ client/src/components/layout/home/HomeOne.js
@@ -3,7 +3,20 @@ import React from "react";
 // components
 
 const HomeOne = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home-one">
+      <div className="home-one__content">
+        <h1 className="home-one__h1">
+          Dobrodosli u nas kutak u kome smo oziveli najbolje u nastavi
+          engleskog.
+        </h1>
+        <p className="home-one__p">
+          Na kreativan i zabavan nacin vam pomazemo da uspesno usvojite engleski
+          i da govorite bez blokade i treme.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default HomeOne;
