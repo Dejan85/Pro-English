66d7f62c237f11e0339b8bffc3277de9f7bd7a5a
unknown <xypnotica@gmail.com>
2019-06-14 16:36:40 +0200

    making structure for home page

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -2,15 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 // components
-import Landing from "./components/layout/landing/Landing";
-// import Course from "./components/layout/courses/Course";
+import LandingOne from "./components/layout/landing/LandingOne";
 
 const MainRouter = () => {
   return (
     <>
       {/* main page */}
-      <Route exact path="/" component={Landing} />
-      {/* <Route exact path="/" component={Course} /> */}
+      <Route exact path="/" component={LandingOne} />
     </>
   );
 };
---
+++ client/src/components/layout/landing/LandingOne.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingOne = () => {
+  return <div className="landing">index page</div>;
+};
+
+export default LandingOne;
