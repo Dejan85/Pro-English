1c9af9614e83ad5bb1d91cda54b95257c6630360
unknown <xypnotica@gmail.com>
2019-06-18 13:27:58 +0200

    events almost done

--- client/src/components/layout/home/HomeEvents.js
+++ client/src/components/layout/home/HomeEvents.js
@@ -27,6 +27,39 @@ const HomeEvent = () => {
       </div>
       <div className="homeEvent__upcoming">
         <h3 className="homeEvent__h3">Buduci dogadjaji</h3>
+        <div className="homeEvent__upcoming__content">
+          <ul className="homeEvent__upcoming__list">
+            <li className="homeEvent__upcoming__item">
+              <p className="homeEvent__upcoming__p">
+                Casovi aktivne konverzacije
+              </p>
+              <span className="homeEvent__upcoming__span">
+                Ponedeljak, 19:00 - 21:00
+              </span>
+            </li>
+            <li className="homeEvent__upcoming__item">
+              <p className="homeEvent__upcoming__p">Priprema za ispite</p>
+              <span className="homeEvent__upcoming__span">
+                Ponedeljak, 19:00 - 21:00
+              </span>
+            </li>
+            <li className="homeEvent__upcoming__item">
+              <p className="homeEvent__upcoming__p">Induvidualni casovi</p>
+              <span className="homeEvent__upcoming__span">
+                Ponedeljak, 19:00 - 21:00
+              </span>
+            </li>
+            <li className="homeEvent__upcoming__item">
+              <p className="homeEvent__upcoming__p">Casovi C1 nivoa</p>
+              <span className="homeEvent__upcoming__span">
+                Ponedeljak, 19:00 - 21:00
+              </span>
+            </li>
+            <Link className="homeEvent__link-2" to="/">
+              Vidi sve
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
