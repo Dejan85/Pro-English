8bc8aa1f78b69d3bc35fbbb236a6c837ccbbd379
unknown <xypnotica@gmail.com>
2019-06-18 10:43:40 +0200

    calendar almost done

--- client/src/components/layout/home/calendar/CalendarEventCard.js
+++ client/src/components/layout/home/calendar/CalendarEventCard.js
@@ -5,15 +5,17 @@ const CalendarEventCard = () => {
     <>
       <div className="calendar__event__card">
         <div className="calendar__event__day">18</div>
-        <div className="calendar__event__month">jun</div>
+        <div className="calendar__event__month">
+          <p>jun</p>
+        </div>
       </div>
       <div className="calendar__event__info">
-        <h5 className="calendar__event__h5">Cascovi aktivne konverzacije</h5>
-        <i className="far fa-clock" />
-        <h5 className="calendar__event__h5">Cascovi aktivne konverzacije</h5>
+        <h5 className="calendar__event__h5">
+          Priprema za polaganje medjunarodnih ispita
+        </h5>
         <p className="calendar__event__p">
-          18. Jun 2010 10:00 - <br />
-          18. Jun 2010 12:30
+          <i className="far fa-clock" />
+          10:00 - 12:30
         </p>
       </div>
     </>
