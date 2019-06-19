fb0b24df198e601604a1765a842ec6cb9c94d4ad
unknown <xypnotica@gmail.com>
2019-06-19 23:28:36 +0200

    reparing newsletter

--- client/src/components/layout/home/partials/NewsLetter.js
+++ client/src/components/layout/home/partials/NewsLetter.js
@@ -4,7 +4,10 @@ const NewsLetter = () => {
   return (
     <div className="newsLetter">
       <form className="newsLetter__form">
-        <input className="newsLetter__input" placeholder="Newsletter" />
+        <input
+          className="newsLetter__input"
+          placeholder="Uzmite svoju dozu znanja..."
+        />
         <button className="newsLetter__btn">Prijavi se</button>
       </form>
     </div>
