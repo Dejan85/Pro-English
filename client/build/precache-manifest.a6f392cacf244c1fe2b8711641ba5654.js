67249624c56492fca423d910be7f2a0d28ab2bd0
unknown <xypnotica@gmail.com>
2019-06-24 22:50:52 +0200

    create build folder fro asw deploy

---
+++ client/build/precache-manifest.a6f392cacf244c1fe2b8711641ba5654.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd72c164b4700864bd626e933352048c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e513116c03b897145a51",
+    "url": "/static/css/main.93c58aa3.chunk.css"
+  },
+  {
+    "revision": "842d380d032950d981d3",
+    "url": "/static/js/2.b708fffe.chunk.js"
+  },
+  {
+    "revision": "e513116c03b897145a51",
+    "url": "/static/js/main.7556cfee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f17d9e71c4e6922b076a6532c32df468",
+    "url": "/static/media/1.f17d9e71.jpg"
+  },
+  {
+    "revision": "2b1d6d4b4c5aa4110446a966485bffac",
+    "url": "/static/media/14.2b1d6d4b.jpg"
+  },
+  {
+    "revision": "37acdc053cd647578f28f95515ad5457",
+    "url": "/static/media/28.37acdc05.webp"
+  },
+  {
+    "revision": "2d2c168b508cee4090e8ff2eed9cf112",
+    "url": "/static/media/29.2d2c168b.webp"
+  },
+  {
+    "revision": "f876c061c12c99a28c28cdd053dfec5a",
+    "url": "/static/media/3.f876c061.jpg"
+  },
+  {
+    "revision": "85bda58d53cde580a48a375a6b59cdce",
+    "url": "/static/media/30.85bda58d.jpg"
+  },
+  {
+    "revision": "2655fe81aa61cef95073502547a7251a",
+    "url": "/static/media/31.2655fe81.jpg"
+  },
+  {
+    "revision": "2e1fb3a15e79b3301c39809bb25fbdef",
+    "url": "/static/media/33.2e1fb3a1.jpg"
+  },
+  {
+    "revision": "5cafd4ae8f1e743449634a6cccdb4bce",
+    "url": "/static/media/34.5cafd4ae.jpg"
+  },
+  {
+    "revision": "614fd2bd044f8ab88e99bb20febc0669",
+    "url": "/static/media/38.614fd2bd.jpg"
+  },
+  {
+    "revision": "c7d10b920a628f3772072d6c48b3ddfb",
+    "url": "/static/media/38.c7d10b92.png"
+  },
+  {
+    "revision": "b9df6284725ecd13d891701bd7255ae8",
+    "url": "/static/media/5.b9df6284.jpg"
+  },
+  {
+    "revision": "6f597bee1da5a663500d33bbb5849461",
+    "url": "/static/media/7.6f597bee.jpg"
+  },
+  {
+    "revision": "f155a6995b59ec2105f0f93086bd5654",
+    "url": "/static/media/9.f155a699.jpg"
+  },
+  {
+    "revision": "cb572717261d219006334c7a95cb4bbd",
+    "url": "/static/media/logo_2.cb572717.jpg"
+  },
+  {
+    "revision": "d9da39e8b8df4131a98c36431429b435",
+    "url": "/static/media/welcome.d9da39e8.png"
+  }
+]);
