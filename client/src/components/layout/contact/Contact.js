0cb92d004c2fc2e14aa4224be2921ceb31b2dedc
unknown <xypnotica@gmail.com>
2019-06-26 22:14:40 +0200

    contact form almost done

--- client/src/components/layout/contact/Contact.js
+++ client/src/components/layout/contact/Contact.js
@@ -13,7 +13,36 @@ const Contact = () => {
       <div className="contact__map">
         <Map />
       </div>
-      <div className="contact__content">content</div>
+      <div className="contact__content">
+        <h1 className="contact__h1">Kontakt</h1>
+        <span className="contact__span">
+          Pisite nam o svemu sto vas interesuje, tu smo da odgovorimo na svako
+          vase pitanje.
+        </span>
+        <div className="contact__form">
+          <form>
+            <div className="contact__input">
+              <label>Ime</label>
+              <input />
+            </div>
+            <div className="contact__input">
+              <label>Email</label>
+              <input />
+            </div>
+            <div className="contact__input">
+              <label>Tema</label>
+              <input />
+            </div>
+            <div className="contact__input">
+              <label>Pitanje</label>
+              <textarea rows="7" />
+            </div>
+            <div className="contact__input">
+              <button>Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
