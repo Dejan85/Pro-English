d1862a11b543b8319b78c94d0c716980189815fd
unknown <xypnotica@gmail.com>
2019-06-29 17:38:29 +0200

    signin and signup style done

--- client/src/components/layout/admin/auth/Signin.js
+++ client/src/components/layout/admin/auth/Signin.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Signin = () => {
-  return <div className="signin">signin</div>;
+  return (
+    <div className="sign">
+      <h1 className="sign__h1">Login</h1>
+      <div className="sign__form">
+        <form>
+          <div className="sign__inputHolder">
+            <label className="sign__label">Email</label>
+            <input className="sign__input" />
+          </div>
+
+          <div className="sign__inputHolder">
+            <label className="sign__label">Password</label>
+            <input className="sign__input" />
+          </div>
+
+          <div className="sign__btnHolder">
+            <button className="sign__btn">Registruj se</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- client/src/components/layout/admin/auth/Signup.js
+++ client/src/components/layout/admin/auth/Signup.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Signup = () => {
-  return <div className="signup">signup</div>;
+  return (
+    <div className="sign">
+      <h1 className="sign__h1">Registracija</h1>
+      <div className="sign__form">
+        <form>
+          <div className="sign__inputHolder">
+            <label className="sign__label">Name</label>
+            <input className="sign__input" />
+          </div>
+          <div className="sign__inputHolder">
+            <label className="sign__label">Prezime</label>
+            <input className="sign__input" />
+          </div>
+          <div className="sign__inputHolder">
+            <label className="sign__label">Email</label>
+            <input className="sign__input" />
+          </div>
+          <div className="sign__inputHolder">
+            <label className="sign__label">Password</label>
+            <input className="sign__input" />
+          </div>
+          <div className="sign__inputHolder">
+            <label className="sign__label">Confirm Password</label>
+            <input className="sign__input" />
+          </div>
+          <div className="sign__btnHolder">
+            <button className="sign__btn">Registruj se</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
