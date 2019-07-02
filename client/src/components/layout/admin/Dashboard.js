f03131a17f0fd0f5771f739f9134db27a35ac185
unknown <xypnotica@gmail.com>
2019-07-03 00:05:48 +0200

    create handler drop menu hooks

--- client/src/components/layout/admin/Dashboard.js
+++ client/src/components/layout/admin/Dashboard.js
@@ -1,17 +1,21 @@
 import React, { useRef } from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import usePrivateRoute from "../../hooks/auth/usePrivateRoute";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 // components
 import DropMenu from "./partials/DropMenu";
 import AddBlog from "./partials/blog/AddBlog";
 
+// hooks
+import usePrivateRoute from "../../hooks/auth/usePrivateRoute";
+import useDropMenu from "../../hooks/auth/useDropMenu";
+
 const Dashboard = () => {
-  const blogRef = useRef();
-  const dropMenuHandler = e => {
-    blogRef.current.classList.toggle("toggle");
-  };
   const { PrivateRoute } = usePrivateRoute();
+  const { dropMenuHandler } = useDropMenu();
+  const blogRef = useRef();
+  // const dropMenuHandler = e => {
+  //   blogRef.current.classList.toggle("toggle");
+  // };
 
   return (
     <Router>
@@ -23,7 +27,7 @@ const Dashboard = () => {
           <div className="dashboard__nav">
             <ul
               className="dashboard__list"
-              onClick={dropMenuHandler}
+              onClick={dropMenuHandler.bind(this, blogRef)}
               ref={blogRef}>
               <li className="dashboard__item">
                 <div className="dashboard__icon">
