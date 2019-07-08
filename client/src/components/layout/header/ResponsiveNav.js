3edade46be2f76093d9d59a70732483a8420054d
unknown <xypnotica@gmail.com>
2019-07-08 14:24:18 +0200

    working on responsiv nav

--- client/src/components/layout/header/ResponsiveNav.js
+++ client/src/components/layout/header/ResponsiveNav.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 import ResponsiveDropNav from "./partials/ResponsiveDropNav";
 
 const ResponsiveNav = () => {
+  const [nav, setNav] = useState(false);
+  const dropNavHandler = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="header__responsiveNav">
-      <i className="fas fa-bars" />
-      <ResponsiveDropNav />
+      <i className="fas fa-bars" onClick={dropNavHandler} />
+      {nav && <ResponsiveDropNav />}
     </div>
   );
 };
