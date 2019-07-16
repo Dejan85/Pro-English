7f0b1ce63a35d126fd34ca44709324f8d76fcc86
unknown <xypnotica@gmail.com>
2019-07-16 03:13:28 +0200

    working on event

--- client/src/components/layout/events/Events.js
+++ client/src/components/layout/events/Events.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-// import Event from './partials/Event';
+import Event from './partials/Event';
 
 const Events = () => {
   const [reset] = useState(() => {
@@ -10,18 +10,24 @@ const Events = () => {
     <div className='events'>
       {reset}
       <div className='events__search'>
-        <div className='events__inputHolder'>
-          <input />
-          <button>Pretrazi dogadjaje</button>
-        </div>
-        <div className='events__sort'>
-          <ul className='events__list'>
-            <li className='events__item'>Mesecni</li>
-            <li className='events__item'>Nedeljni</li>
-            <li className='events__item'>Dnevni</li>
-          </ul>
-        </div>
+        <p className='events__label'>Izaberi daatum</p>
+        <div className='events__calendar'>07/25/2019</div>
+        <button className='events__button'>Pretrazi Dogadjaje</button>
       </div>
+      <div className='events__sort'>
+        <ul className='events__list'>
+          <li className='events__item'>Aktuelno</li>
+          <li className='events__item'>Dnevni</li>
+          <li className='events__item'>Nedeljni</li>
+          <li className='events__item'>Mesecni</li>
+        </ul>
+      </div>
+      <div className='events__heading'>
+        <h2 className='events__h2'>Dogadjaji za 16. 07. 2019.</h2>
+      </div>
+      <Event />
+      <Event />
+      <Event />
     </div>
   );
 };
--- client/src/components/layout/events/partials/Event.js
+++ client/src/components/layout/events/partials/Event.js
@@ -1,31 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-//components
+// components
 
 const Event = () => {
   return (
-    <div className="event">
-      <div className="event__date">
-        <div className="event__date--num">28</div>
-        <div className="event__date--mounth">JULY</div>
-      </div>
-      <div className="event__content">
-        <h2 className="event__h2">
+    <div className='event'>
+      <div className='event__content'>
+        <div className='event__date'>
+          <div className='event__date--num'>28</div>
+          <div className='event__date--mounth'>JULY</div>
+        </div>
+        <h2 className='event__h2'>
           Priprema za polaganje medjunarodnih ispita
+          <span className='event__span'>
+            <i className='far fa-clock' />
+            October 8, 2018 10:00 - March 31, 2020 17:00
+          </span>
         </h2>
-        <span className="event__span">
-          <i className="far fa-clock" />
-          October 8, 2018 10:00 - March 31, 2020 17:00
-        </span>
-        <p className="event__p">
+
+        <p className='event__p'>
           Sed rutrum, libero non pretium tristique, arcu mi sollicitudin ex,
           quis venenatis orci tellus vel dolor. Donec gravida, dolor ut auctor
           facilisis, enim dolor pellentesque lectus, nec vehicula nibh risus ac
           leo. Mauris volutpat aliquam tellus nec rhoncus. Aliquam et nibh
-          pulvinar, sodales nibh et, pretium urna.
+          pulvinar, sodales nibh et, pretium urna. Sed rutrum, libero non
+          pretium tristique, arcu mi sollicitudin ex, quis venenatis orci tellus
+          vel dolor. Donec gravida, dolor ut auctor facilisis, enim dolor
+          pellentesque lectus, nec vehicula nibh risus ac leo. Mauris volutpat
+          aliquam tellus nec rhoncus. Aliquam et nibh pulvinar, sodales nibh et,
+          pretium urna.
         </p>
-        <Link to="/dogadjaji/one" className="event__link">
+        <Link to='/dogadjaji/one' className='event__link'>
           Procitaj sve
         </Link>
       </div>
