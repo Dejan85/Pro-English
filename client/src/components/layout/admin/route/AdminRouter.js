887386a55b9c58536599d8176ecdafb62a2a1dfd
unknown <xypnotica@gmail.com>
2019-07-18 01:38:02 +0200

    admin route fix

--- client/src/components/layout/admin/route/AdminRouter.js
+++ client/src/components/layout/admin/route/AdminRouter.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { Route, withRouter } from "react-router-dom";
+import React, { useState } from 'react'
+import { Route, withRouter, Redirect } from 'react-router-dom'
 
-//hooks
-// import usePrivateRoute from "../../../hooks/auth/usePrivateRoute";
-// import useAuthenticate from "../../../hooks/auth/useAuthenticate";
+// hooks
+// import usePrivateRoute from '../../../hooks/auth/usePrivateRoute'
+import useAuthenticate from '../../../hooks/auth/useAuthenticate'
 
 // admin
-// import Dashboard from "../Dashboard";
-import Signin from "../auth/Signin";
-import Signup from "../auth/Signup";
+import Dashboard from '../Dashboard'
+import Signin from '../auth/Signin'
+import Signup from '../auth/Signup'
 
 const AdminRouter = props => {
-  // const { PrivateRoute } = usePrivateRoute();
-  // const { isAuthenticated } = useAuthenticate();
+  // const { PrivateRoute } = usePrivateRoute()
+  const { isAuthenticated } = useAuthenticate()
+  const [dashboardHandler] = useState(() => {
+    return (
+      props.history.location.pathname === '/admin' ||
+      props.history.location.pathname === '/admin/blog'
+    )
+  })
 
   return (
     <>
-      {/* {(isAuthenticated() && <Dashboard />) ||
-        (!isAuthenticated() && <Redirect to="/admin/signin" />)} */}
-      <Route exact path="/admin/signin" component={Signin} />
-      <Route exact path="/admin/signup" component={Signup} />
+      {(isAuthenticated() && dashboardHandler && <Dashboard />) ||
+        (!isAuthenticated() && <Redirect to='/admin/signin' />)}
+      <Route exact path='/admin/signin' component={Signin} />
+      <Route exact path='/admin/signup' component={Signup} />
     </>
-  );
-};
+  )
+}
 
-export default withRouter(AdminRouter);
+export default withRouter(AdminRouter)
