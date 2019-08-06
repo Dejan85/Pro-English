38a8939ca132d13b5d0c611b26bd9dc35f211d24
unknown <xypnotica@gmail.com>
2019-08-06 02:33:20 +0200

    workgin on exams

--- client/src/components/layout/exams/Exams.js
+++ client/src/components/layout/exams/Exams.js
@@ -8,35 +8,21 @@ const Exams = () => {
   return (
     <div className='exams'>
       {reset}
-      {/* <h3 className='blog__h3'>Pripreme za ispite</h3>
-      <div className='blog__line' /> */}
       <div className='exams__content'>
-        {/* <ul className="exams__list">
-          <li className="exams__item">Utvrdite nivo znanja</li>
-          <li className="exams__item">GRE</li>
-          <li className="exams__item">BOCCONI</li>
-          <li className="exams__item">GMAT</li>
-          <li className="exams__item">GREE</li>
-        </ul> */}
         <div className='exams__list1'>
-          <div className='exams__list1--tab'>Nivo znanja</div>
-          <div className='exams__list1--content' />
+          <div className='exams__list1--tab'>GRE</div>
         </div>
+
         <div className='exams__list2'>
-          <div className='exams__list2--tab'>GRE</div>
-          <div className='exams__list2--content' />
+          <div className='exams__list2--tab'>GREE</div>
         </div>
+
         <div className='exams__list3'>
-          <div className='exams__list3--tab'>BOCCONI</div>
-          <div className='exams__list3--content' />
+          <div className='exams__list3--tab'>GMAT</div>
         </div>
+
         <div className='exams__list4'>
-          <div className='exams__list4--tab'>GMAT</div>
-          <div className='exams__list4--content' />
-        </div>
-        <div className='exams__list5'>
-          <div className='exams__list5--tab'>GREE</div>
-          <div className='exams__list5--content' />
+          <div className='exams__list4--tab'>BOCCONI</div>
         </div>
       </div>
     </div>
