daeb674aaa9da344b7d2a6f7c59cd247dbb2d97a
unknown <xypnotica@gmail.com>
2019-08-09 02:37:51 +0200

    remove two empty component

--- client/src/components/layout/about/HowWeWork.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HowWeWork = () => {
-  return <div className="about__wowWeWork">HowWeWork</div>;
-};
-
-export default HowWeWork;
--- client/src/components/layout/about/WhoWeAre.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WhoEeArr = () => {
-  return <div className="about__whoEeArr">WhoEeArr</div>;
-};
-
-export default WhoEeArr;
