740eeda4fd3889d13a54c0581b66aa8a42958e11
xypnotica <xypnotica@gmail.com>
2019-08-25 03:34:27 +0200

    change style on exams page/tabs

--- client/src/components/hooks/exams/useTabs.js
+++ client/src/components/hooks/exams/useTabs.js
@@ -1,30 +1,40 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
 const useTabs = () => {
   const gre = useRef();
   const ielts = useRef();
   const gmat = useRef();
   const sat = useRef();
-  const toefil = useRef();
+  const teofl = useRef();
   const bocconi = useRef();
 
   const tabs = e => {
-    if (e.target.getAttribute('data-name') === 'gre') {
-      gre.current.style.setProperty('--width', '82%');
-      ielts.current.style.setProperty('--width', '87%');
-      gmat.current.style.setProperty('--width', '92%');
-    } else if (e.target.getAttribute('data-name') === 'ielts') {
-      gre.current.style.setProperty('--width', '1%');
-      ielts.current.style.setProperty('--width', '87%');
-      gmat.current.style.setProperty('--width', '92%');
-    } else if (e.target.getAttribute('data-name') === 'gmat') {
-      gre.current.style.setProperty('--width', '1%');
-      ielts.current.style.setProperty('--width', '1%');
-      gmat.current.style.setProperty('--width', '92%');
-    } else if (e.target.getAttribute('data-name') === 'bocconi') {
-      gre.current.style.setProperty('--width', '1%');
-      ielts.current.style.setProperty('--width', '1%');
-      gmat.current.style.setProperty('--width', '1%');
+    if (e.target.getAttribute("data-name") === "gre") {
+      gre.current.style.setProperty("--width", "82%");
+      ielts.current.style.setProperty("--width", "87%");
+      gmat.current.style.setProperty("--width", "92%");
+    } else if (e.target.getAttribute("data-name") === "ielts") {
+      gre.current.style.setProperty("--width", "1%");
+      ielts.current.style.setProperty("--width", "87%");
+      gmat.current.style.setProperty("--width", "92%");
+    } else if (e.target.getAttribute("data-name") === "gmat") {
+      gre.current.style.setProperty("--width", "1%");
+      ielts.current.style.setProperty("--width", "1%");
+      gmat.current.style.setProperty("--width", "92%");
+    } else if (e.target.getAttribute("data-name") === "bocconi") {
+      gre.current.style.setProperty("--width", "1%");
+      ielts.current.style.setProperty("--width", "1%");
+      gmat.current.style.setProperty("--width", "1%");
+    } else if (e.target.getAttribute("data-name") === "sat") {
+      gre.current.style.setProperty("--width", "1%");
+      ielts.current.style.setProperty("--width", "1%");
+      gmat.current.style.setProperty("--width", "1%");
+      sat.current.style.setProperty("--width", "1%");
+    } else if (e.target.getAttribute("data-name") === "teofl") {
+      gre.current.style.setProperty("--width", "1%");
+      ielts.current.style.setProperty("--width", "1%");
+      gmat.current.style.setProperty("--width", "1%");
+      teofl.current.style.setProperty("--width", "1%");
     }
   };
 
@@ -33,7 +43,9 @@ const useTabs = () => {
     gre,
     ielts,
     gmat,
-    bocconi
+    bocconi,
+    sat,
+    teofl
   };
 };
 
--- client/src/components/layout/exams/Exams.js
+++ client/src/components/layout/exams/Exams.js
@@ -8,13 +8,14 @@ import useTabs from "../../hooks/exams/useTabs";
 
 const Exams = props => {
   const { exams } = props;
-
   const [reset] = useState(() => {
     return window.scrollTo(0, 0);
   });
 
   // tabs is onClick handler, and gre, ielts, gmat bocconi is ref
-  const { tabs, gre, ielts, gmat, bocconi } = useTabs();
+  const { tabs } = useTabs();
+  const { gre, ielts, gmat, bocconi, sat, teofl } = useTabs();
+  const refs = [gre, ielts, gmat, bocconi, sat, teofl];
 
   return (
     <div className="exams">
@@ -23,9 +24,13 @@ const Exams = props => {
         {exams &&
           exams.map((item, index) => {
             return (
-              <div className="exams__list1" ref={gre} key={index}>
+              <div
+                className={`exams__list${index}`}
+                ref={refs[index]}
+                key={index}
+              >
                 <div
-                  className="exams__list1--tab"
+                  className={`exams__list${index}--tab`}
                   data-name="gre"
                   onClick={tabs}
                 >
@@ -41,143 +46,6 @@ const Exams = props => {
               </div>
             );
           })}
-        {/* <div className="exams__list1" ref={gre}>
-          <div className="exams__list1--tab" data-name="gre" onClick={tabs}>
-            GRE
-          </div>
-          <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
-            <div className="exams__body">
-              {exams.gre &&
-                exams.gre.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      <h1 className="exams__h1">{item.h1}</h1>
-                      <p className="exams__p">{item.p}</p>
-                      <ul className="exams__list">
-                        {item.table &&
-                          item.table.map((item, index) => {
-                            return (
-                              <li key={index} className="exams__item">
-                                {item}
-                              </li>
-                            );
-                          })}
-                      </ul>
-                    </div>
-                  );
-                })}
-            </div>
-          </Scrollbars>
-        </div> */}
-
-        {/* <div className="exams__list2" ref={ielts}>
-          <div className="exams__list2--tab" data-name="ielts" onClick={tabs}>
-            IELTS
-          </div>
-          <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
-            <div className="exams__body">
-              {exams.ielts &&
-                exams.ielts.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      <h1 className="exams__h1">{item.h1}</h1>
-                      {item.p &&
-                        item.p.map((item, index) => {
-                          return (
-                            <p key={index} className="exams__p">
-                              {item}
-                            </p>
-                          );
-                        })}
-                      <ul className="exams__list">
-                        {item.list &&
-                          item.list.map((item, index) => {
-                            return (
-                              <li key={index} className="exams__item">
-                                {item}
-                              </li>
-                            );
-                          })}
-                      </ul>
-                      {item.arr &&
-                        item.arr.map((item, index) => {
-                          return (
-                            <div key={index}>
-                              <h2 className="exams__h2">{item.h2}</h2>
-
-                              {item.p &&
-                                item.p.map((item, index) => {
-                                  return (
-                                    <p key={index} className="exams__p">
-                                      {item}
-                                    </p>
-                                  );
-                                })}
-
-                              {item.h3 && (
-                                <h3 className="exams__h3">{item.h3}</h3>
-                              )}
-                              <ul className="exams__list">
-                                {item.list &&
-                                  item.list.map((item, index) => {
-                                    return (
-                                      <li key={index} className="exams__item">
-                                        {item}
-                                      </li>
-                                    );
-                                  })}
-                              </ul>
-                            </div>
-                          );
-                        })}
-                    </div>
-                  );
-                })}
-            </div>
-          </Scrollbars>
-        </div>
-
-        <div className="exams__list3" ref={gmat}>
-          <div className="exams__list3--tab" data-name="gmat" onClick={tabs}>
-            GMAT
-          </div>
-          <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
-            <div className="exams__body">
-              {exams.gmat &&
-                exams.gmat.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      {item.h1 && <h1 className="exams__h1">{item.h1}</h1>}
-                      {item.p && <p className="exams__p">{item.p}</p>}
-
-                      {item.h3 && <h2 className="exams__h2">{item.h3}</h2>}
-                      <ul className="exams__list">
-                        {item.list &&
-                          item.list.map((item, index) => {
-                            return (
-                              <li key={index} className="exams__item">
-                                {item}
-                              </li>
-                            );
-                          })}
-                      </ul>
-                    </div>
-                  );
-                })}
-            </div>
-          </Scrollbars>
-        </div>
-
-        <div className="exams__list4" ref={bocconi}>
-          <div className="exams__list4--tab" data-name="bocconi" onClick={tabs}>
-            {examsTest && examsTest.item.title}
-          </div>
-          <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
-            <div className="exams__body">
-              {examsTest && parse(examsTest.item.body)}
-            </div>
-          </Scrollbars>
-        </div> */}
       </div>
     </div>
   );
