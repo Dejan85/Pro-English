5a9619e7fed58046f242927d730b0b58caf5c1c6
xypnotica <xypnotica@gmail.com>
2019-09-18 17:32:30 +0200

    ubacujem srpska slova

--- client/src/components/hooks/home/useBox.js
+++ client/src/components/hooks/home/useBox.js
@@ -8,37 +8,37 @@ const useBox = () => {
   const [box1] = useState({
     i: <i className="fas fa-users" />,
     p: "Rad u malim grupama",
-    span:
-      "Predavanja vodimo u malim grupama. Zelimo da posvetimo maksimum paznje svakom u grupi.",
-    color: "home__icon--box1",
+    // span:
+    //   "Predavanja vodimo u malim grupama. Zelimo da posvetimo maksimum paznje svakom u grupi.",
+    color: "home__icon--box1"
   });
   const [box2] = useState({
     i: <i className="far fa-comments" />,
     p: "Aktivna upotreba",
-    span:
-      " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
-    color: "home__icon--box2",
+    // span:
+    //   " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
+    color: "home__icon--box2"
   });
   const [box3] = useState({
     i: <i className="fas fa-user-tie" />,
     p: "Induvidualni pristup",
-    span:
-      " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
-    color: "home__icon--box3",
+    // span:
+    //   " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
+    color: "home__icon--box3"
   });
   const [box4] = useState({
     i: <i className="far fa-comment-dots" />,
-    p: "Prosirenje vokabulara",
-    span:
-      " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
-    color: "home__icon--box4",
+    p: "Proširenje vokabulara",
+    // span:
+    //   " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
+    color: "home__icon--box4"
   });
   const [box5] = useState({
     i: <i className="fab fa-teamspeak" />,
     p: "Online predavanja",
-    span:
-      " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
-    color: "home__icon--box5",
+    // span:
+    //   " Whether you stay with us for one week or one year, we will make sure you have the time of your life.",
+    color: "home__icon--box5"
   });
 
   //
@@ -49,19 +49,19 @@ const useBox = () => {
     heading: "a2",
     p:
       " This crash course in layers reveals or shows you in photoshop and create wonders in graphics industry...",
-    color: "#FC424F",
+    color: "#FC424F"
   });
   const [circle2] = useState({
     heading: "b1",
     p:
       " This crash course in layers reveals or shows you in photoshop and create wonders in graphics industry...",
-    color: "#59B131",
+    color: "#59B131"
   });
   const [circle3] = useState({
     heading: "c1",
     p:
       " This crash course in layers reveals or shows you in photoshop and create wonders in graphics industry...",
-    color: "#018CE1",
+    color: "#018CE1"
   });
 
   const boxes = () => {
